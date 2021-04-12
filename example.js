e06cd93ab3d282768df9d4167b4afdf5d9274d13
csegundo <carlossn20165@gmail.com>
2021-04-12 21:13:24 +0200

    Simple emoji selector done. Group items

--- example.js
+++ example.js
@@ -13,3 +13,20 @@ $(function(){
         $('input').emojiPicker();
     });
 });
+
+// GET EMOJIS
+// var i = 0;
+// var emojis = [];
+// document.querySelector('table.w3-table-all.charset-tryit tbody tr').forEach(tr => {
+//     if(i > 0){
+//         var j = 0;
+//         tr.querySelectorAll('td').forEach(td => {
+//             if(j == 1){
+//                 emojis.push(td.innerHTML);
+//             }
+//             j++;
+//         });
+//     }
+    
+//     i++;
+// });
